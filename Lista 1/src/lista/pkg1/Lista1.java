74ada0a213a8e4265dd9aebef78b38b28a06fc7b
GabrielP1909 <gabrielneves681p@gmail.com>
2025-09-01 16:39:09 -0300

    primeiro exercício

---
+++ Lista 1/src/lista/pkg1/Lista1.java
@@ -0,0 +1,11 @@
+
+package lista.pkg1;
+
+public class Lista1 {
+
+    public static void main(String[] args) {
+        cumprimentar c1 = new cumprimentar();
+        c1.Cumprimentar();
+    }
+    
+}
---
+++ Lista 1/src/lista/pkg1/cumprimentar.java
@@ -0,0 +1,9 @@
+
+package lista.pkg1;
+
+public class cumprimentar {
+    
+    public void Cumprimentar(){
+        System.out.println("Olá, mundo!");
+    }
+}
