208f66fd9987ab6ab9356203dff457d17e2809d9
KlintBzl <klintscouts@gmail.com>
2025-09-01 17:01:43 -0300

    terceiro exercício

---
+++ Lista 3/src/lista/pkg3/Lista3.java
@@ -0,0 +1,15 @@
+
+package lista.pkg3;
+
+public class Lista3 {
+
+    public static void main(String[] args) {
+        concatenar c1 = new concatenar();
+
+        System.out.println("Primeira palavra: " + c1.getP1());
+        System.out.println("Segunda palavra: " + c1.getP2());
+        System.out.println("");
+                System.out.println("Frase final: " + c1.Concatenar());
+        }
+    
+}
---
+++ Lista 3/src/lista/pkg3/concatenar.java
@@ -0,0 +1,22 @@
+
+package lista.pkg3;
+
+public class concatenar {
+    
+    String p1 = "Olá, ";
+    String p2 = "mundo!";
+    
+    public String Concatenar(){
+        String F = p1 + p2;
+        return F;
+    }
+
+    public String getP1() {
+        return p1;
+    }
+
+    public String getP2() {
+        return p2;
+    }
+    
+}
